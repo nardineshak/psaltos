233c66fda942cc4b46d402e28b069689ba914ce4
nardineshak <neshak@cs.washington.edu>
2023-03-07 19:48:12 -0800

    build error test

--- Psaltos.Tests/UnitTest1.cs
+++ Psaltos.Tests/UnitTest1.cs
@@ -5,10 +5,10 @@ namespace Psaltos.Tests
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
+        
         public void TestMethod1()
         {
-            Assert.AreEqual(true, false);
+            Assert.AreEqual(true, true);
         }
     }
 }
